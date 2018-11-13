8359e1ad340b667ad08793da35f81fcb8562eae3
Markus Pesch <markus.pesch@cryptic.systems>
2018-11-13 22:05:32 +0100

    add: solution exercise 4, task 10

--- sql/uebung_04/aufgabe_10.sql
+++ sql/uebung_04/aufgabe_10.sql
@@ -1,1 +1,31 @@
--- Deine Lösung
+-- Ohne Exist
+SELECT a1.klausurnr
+FROM anmeldung a1
+WHERE (
+  (
+    SELECT COUNT (*) AS "A_G"
+    FROM anmeldung a2
+    WHERE a2.klausurnr = a1.klausurnr
+  ) -
+  (
+    SELECT COUNT (*) AS "A_B"
+    FROM anmeldung a3
+    WHERE a3.klausurnr = a1.klausurnr
+    AND a3.note IS NOT NULL)
+  ) =
+  (
+    SELECT COUNT (*) AS "A_G"
+    FROM anmeldung a4O
+    WHERE a4.klausurnr = a1.klausurnr
+  )
+ORDER BY a1.klausurnr ASC;
+
+-- Mit Exists
+SELECT klausurnr
+FROM anmeldung a
+WHERE NOT EXISTS (
+  SELECT *
+  FROM anmeldung
+  WHERE a.klausurnr = klausurnr
+  AND note IS NOT NULL
+);
