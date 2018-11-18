e67545663966b61e6cbc9543819669899ce94c96
Markus Pesch <markus.pesch@cryptic.systems>
2018-11-18 10:51:02 +0100

    fix: solution exercise 1, task 1,14

--- sql/uebung_01/aufgabe_01.sql
+++ sql/uebung_01/aufgabe_01.sql
@@ -1,8 +1,10 @@
 -- Relativer Pfad (Linux)
-start "~/workspace/tgdb_ws1819l/sql/schema_01.sql"
+-- start "~/workspace/tgdb_ws1819l/sql/schema_01.sql"
 
 -- Absoluter Pfad (Linux)
-start "/home/markus/workspace/tgdb_ws1819l/sql/schema_01.sql"
+-- start "/home/markus/workspace/tgdb_ws1819l/sql/schema_01.sql"
 
 -- Absoluter Pfad (Windows)
-start "C:/Users/Markus/workspace/tgdb_ws1819l/sql/schema_01.sql"
+-- start "C:/Users/Markus/workspace/tgdb_ws1819l/sql/schema_01.sql"
+
+-- Auskommentiert wegen make execute-solutions
--- sql/uebung_01/aufgabe_14.sql
+++ sql/uebung_01/aufgabe_14.sql
@@ -10,10 +10,10 @@ OR metal_type = 'Geschliffen';
 SELECT DISTINCT m.metal_name
 FROM metals m
 WHERE m.metal_type IN ('Träne', 'Geschliffen')
-AND m.metal_propery = '1.4301';
+AND m.metal_property = '1.4301';
 
 SELECT DISTINCT m.metal_name
 FROM metals m
 WHERE (m.metal_type = 'Träne'
 OR m.metal_type = 'Geschliffen')
-AND m.metal_propery = '1.4301';
+AND m.metal_property = '1.4301';
